8f061a71006570e7c7fb12e7bc0bc3eecf7c9b1f
Igor Vieira <igor.p.r.vieira@gmail.com>
2021-04-17 10:16:20 -0300

    add new cases in this tests

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,38 +1,49 @@
-import { User } from "@src/models/user"
+import { User } from '@src/models/user';
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
-    await User.deleteMany({})
-  })
-
+    await User.deleteMany({});
+  });
   describe('When creating a new user', () => {
     it('should successfully create a new user', async () => {
       const newUser = {
         name: 'John Doe',
-        email: 'john@email.com',
+        email: 'john@mail.com',
         password: '1234',
-      }
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
 
-      const response  = await global.testRequest.post('/users').send(newUser)
+    it('Should return 422 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
 
-      expect(response.status).toBe(201)
-      expect(response.body).toEqual(expect.objectContaining(newUser))
-    })
-  })
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
 
-  // it('Should return 400 when there is a validation error', async () => {
-  //   const newUser = {
-  //     email: 'john@email.com',
-  //     password: '1234',
-  //   }
+    it('Should return 409 when the email already exists', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      await global.testRequest.post('/users').send(newUser);
+      const response = await global.testRequest.post('/users').send(newUser);
 
-  //   const response  = await global.testRequest.post('/users').send(newUser)
-
-  //   expect(response.status).toBe(400)
-  //   expect(response.body).toEqual({
-  //     code: 400,
-  //     error: 'User validation failed: name Path `name` is required'
-  //   })
-  // })
-
-})
+      expect(response.status).toBe(409);
+      expect(response.body).toEqual({
+        code: 409,
+        error: 'User validation failed: email: already exists in the database.',
+      });
+    });
+  });
+});
